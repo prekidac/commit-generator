73839938325da66829d070203b9185950b7baae6
prekidac <al.slavisa@gmail.com>
2021-05-10 08:32:00 +0200

    ➖ remove(main): description from type hint
    It's self explanatory

--- commit-generator.py
+++ commit-generator.py
@@ -29,12 +29,11 @@ class Commit(object):
         with open(config, "r") as f:
             self.config = json.load(f)
 
-    def all_types(self) -> dict:
-        """return: {type: description}"""
-        dct = {}
+    def all_types(self) -> list:
+        lst = []
         for i in self.config["gitmojis"]:
-            dct[i["type"]] = i["description"]
-        return dct
+            lst.append(i["type"])
+        return lst
 
     def prompt(self, prompt: str, optional: bool = False) -> str:
         while True:
@@ -43,19 +42,17 @@ class Commit(object):
                 return i
 
     def commit_type(self) -> None:
-        dct = self.all_types()
+        lst = self.all_types()
         while True:
             self.type = self.prompt(f"Type of change: ")
-            if self.type.rstrip("!") in dct.keys():
+            if self.type.rstrip("!") in lst:
                 if self.type[-1] == "!":
                     self.breaking_change = True
                     self.type = self.type.rstrip("!")
                 else:
                     self.breaking_change = False
                 break
-            for key, value in dct.items():
-                print(
-                    f"{fg('red')+attr('bold')+key+attr('reset')+fg('blue')+' - '+value+attr('reset')}")
+            print(fg('blue')+" ".join(lst)+attr('reset'))
 
     def commit_scope(self) -> None:
         lst = self.config["scopes"]
@@ -99,7 +96,8 @@ class Commit(object):
 
     def commit_footer(self) -> None:
         if self.prompt("API change [y|n]: ", optional=True) == "y":
-            self.breaking_change = self.form_lines(self.prompt("Describe change: "))
+            self.breaking_change = self.form_lines(
+                self.prompt("Describe change: "))
         else:
             self.breaking_change = False
 
